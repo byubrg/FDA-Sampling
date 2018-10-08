b95aadf51d850b224d39d6a6337ceaa1d099ea24
LukeSagers <lukewsagers@gmail.com>
2018-10-08 11:22:50 -0600

    Add random forests classifier

--- learner_functions.py
+++ learner_functions.py
@@ -6,13 +6,25 @@ Contained in file are functions used for
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit
 from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 RAND_STATE = 0
 TEST_SIZE = 0.3
 NUMBER_OF_SPLITS = 10
 SCORING_METHOD = 'accuracy'
 
+def train_classifier(data, labels, classifier, **kwargs):
+    model = classifier(**kwargs)
+    cv = StratifiedShuffleSplit(
+        n_splits=NUMBER_OF_SPLITS,
+        test_size=TEST_SIZE,
+        random_state=RAND_STATE
+    )
+    scores = cross_val_score(model, data, labels, cv=cv, scoring=SCORING_METHOD)
+    print(scores)
 
+def train_rf(data, labels):
+    train_classifier(data, labels, RandomForestClassifier, n_estimators=5)
 
 def train_knn(data,labels):
     knn = KNeighborsClassifier()
@@ -36,4 +48,3 @@ def train_sgd(data, labels):
 
     scores = cross_val_score(sgd, data, labels, cv=cv, scoring=SCORING_METHOD)
     print(scores)
-
