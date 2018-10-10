c391d5df5b8ad0df8b12847ec869e519cd9ef970
Michael Bradshaw <michaelbradshaw@PG-WebDev-Michael.byu.edu>
2018-10-10 10:33:29 -0600

    created a cmobind data set for clinical and mass spec data combind, added several new learner function

--- learner_functions.py
+++ learner_functions.py
@@ -6,16 +6,18 @@ Contained in file are functions used for
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit
 from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.ensemble import BaggingClassifier
 
 RAND_STATE = 0
-TEST_SIZE = 0.3
-NUMBER_OF_SPLITS = 10
+TEST_SIZE = 0.1
+NUMBER_OF_SPLITS = 100
 SCORING_METHOD = 'accuracy'
 
 
 
 def train_knn(data,labels):
-    knn = KNeighborsClassifier()
+    knn = KNeighborsClassifier(metric='manhattan')
     cv = StratifiedShuffleSplit( 
             n_splits = NUMBER_OF_SPLITS, 
             test_size = TEST_SIZE, 
@@ -23,10 +25,10 @@ def train_knn(data,labels):
     
     scores = cross_val_score(knn, data, labels, cv = cv, scoring = SCORING_METHOD)
     print(scores)
-    
+
 def train_sgd(data, labels):
 
-    sgd = SGDClassifier(shuffle=True)
+    sgd = SGDClassifier()
     sgd.fit(data, labels)
 
     cv = StratifiedShuffleSplit(
@@ -37,3 +39,25 @@ def train_sgd(data, labels):
     scores = cross_val_score(sgd, data, labels, cv=cv, scoring=SCORING_METHOD)
     print(scores)
 
+def train_nc(data,labels):
+    nc = NearestCentroid(metric='manhattan')
+    cv = StratifiedShuffleSplit( 
+            n_splits = NUMBER_OF_SPLITS, 
+            test_size = TEST_SIZE, 
+            random_state = RAND_STATE )
+    
+    scores = cross_val_score(nc, data, labels, cv = cv, scoring = SCORING_METHOD)
+    print(scores)
+
+def train_bagging_knn(data,labels):
+    bagging = BaggingClassifier(KNeighborsClassifier(metric='manhattan',algorithm='brute'),
+                                n_estimators=30,
+                                max_samples=0.25,
+                                max_features=0.25,
+                                warm_start=True)
+    cv = StratifiedShuffleSplit( 
+        n_splits = NUMBER_OF_SPLITS, 
+        test_size = TEST_SIZE)
+    
+    scores = cross_val_score(bagging, data, labels, cv = cv, scoring = SCORING_METHOD)
+    print(scores)
--- main.py
+++ main.py
@@ -3,14 +3,34 @@ import pandas as pd
 
 #read in the training files
 clinical = pd.read_csv('data/raw/train_cli.tsv', sep='\t')
-protein = pd.read_csv('data/raw/train_pro.tsv', sep='\t')
+protein = pd.read_csv('data/raw/train_pro.tsv', sep='\t').T
+labels = pd.read_csv('data/tidy/sum_tab_1.csv', sep=',')
+
+#create the labels for which samples ahve been mislabeled
+mismatch_labels = labels.mismatch.tolist()
+
+#make the row names of clinical data equal to the first column's content
+clinical.index = clinical['sample'].tolist()
+
+#remove the first column
+clinical = clinical.iloc[:,1:]
+
+#create column headers for protein data
+protein.columns = protein.iloc[0]
+protein = protein.iloc[1:]
 
 #impute missing values with 0
 protein = protein.fillna(0)
 clinical = clinical.fillna(0)
 
-#remove the sample labels and transform the data such that proteins are features
-protein_data = protein.iloc[:,1:].T
+#combind teh clinical and the protein data
+joint_data = protein.combine_first(clinical)
+
+#replace the nominal values from the clinical set with continuous values
+joint_data = joint_data.replace('MSI-Low/MSS',0)
+joint_data = joint_data.replace('MSI-High',1)
+joint_data = joint_data.replace('Male',1)
+joint_data = joint_data.replace('Female',0)
 
 #extract single columns of information and convert to a list
 gender_labels = clinical['gender'].tolist()
@@ -20,4 +40,4 @@ MSI_labels = clinical['msi'].tolist()
 clinical['combind'] = clinical['gender'] + clinical['msi']
 combind_labels = clinical['combind'].tolist()
 
-lf.train_knn(protein_data,combind_labels)
+lf.train_bagging_knn(joint_data,mismatch_labels)
