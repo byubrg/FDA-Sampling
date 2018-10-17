7523caca9c705ce8dfe37bc26e052da4b176b1a3
Jonathan Dayton <jonathandayton23@gmail.com>
2018-10-17 17:50:43 -0600

    Add documentation for running feature selection

--- feature_selection.py
+++ feature_selection.py
@@ -3,34 +3,6 @@ Explore scikitlearn's feature selection capabilities.
 """
 from sklearn.feature_selection import VarianceThreshold
 from load_data import LoadData
-
-def normalize(df):
-    """Normalize each column into roughly [-1.0, 1.0] centered around 0.0.
-
-    Arguments:
-        df {pandas.DataFrame} -- The data to normalize. Each column
-            must be quantitative.
-
-    Returns:
-        pandas.DataFrame -- The normalized data.
-    """
-
-    return (df - df.mean()) / (df.max() - df.min())
-
-def fix_data(df):
-    """Preprocess dataframe to fill NaNs with 0s and remove bad
-    columns.
-
-    Arguments:
-        df {pandas.DataFrame} -- DataFrame to be processed.
-
-    Returns:
-        pandas.DataFrame -- Processed dataframe. Note that some columns
-            may be removed.
-    """
-    bad_columns = ["TMEM35A"]
-    return df.fillna(0.0).drop(bad_columns, axis="columns")
-
 def _select_features(df, selector=VarianceThreshold, **kwargs):
     """Select features to be used for classification based on some
     scikit-learn feature selector.
@@ -48,7 +20,6 @@ def _select_features(df, selector=VarianceThreshold, **kwargs):
         list -- The names of the selected features (columns in the
             input DataFrame).
     """
-    df = normalize(df)
     feature_selector = selector(**kwargs).fit(df)
     return df[df.columns[feature_selector.get_support(indices=True)]]
 
@@ -57,5 +28,4 @@ def select(df, threshold=0.125):
 
 if __name__ == "__main__":
     data = LoadData()
-    proteomic = fix_data(data.proteomic)
-    print(select(proteomic, threshold=0.125))
+    print(select(data.proteomic, threshold=0.125))
