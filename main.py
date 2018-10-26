e691b3ca22fac5cc5a82ca77084328f074785f28
Andrew Peterson <andrewpeterson99@gmail.com>
2018-10-26 16:22:44 -0600

    Added updated main.py to use hard_vote and added hard_vote.py
    Added hard_vote.py, uses senate-based voting to determine gender and MSI level.  Returns a new array after comparing results from multiple algorithms (these algorithms are being passed via an array declared in main.py right now).  Each run of function in hard_vote only generates one array, so use the 'type' parameter to specify whether comparing MSI or gender, then assign the results to array accordingly.

---
+++ main.py
@@ -0,0 +1,44 @@
+import learner_functions as lf
+import load_data as ld
+import hard_vote as hv
+
+data = ld.LoadData()
+
+#create test and train labels
+gender_labels = data.clinical['gender'].tolist()
+MSI_labels = data.clinical['msi'].tolist()
+test_gender_labels = data.test_clinical['gender'].tolist()
+test_MSI_labels = data.test_clinical['msi'].tolist()
+
+#train learners for gender and msi here:
+knn_gender = lf.train_knn(data.proteomic,gender_labels)
+knn_msi = lf.train_knn(data.proteomic,MSI_labels)
+lr_gender = lf.train_lr(data.proteomic,gender_labels)
+lr_msi = lf.train_lr(data.proteomic,MSI_labels)
+
+
+
+#make final predictions here, give it the two trained classifiers
+lf.generate_and_write_results(data.test_proteomic.fillna(0.0),
+                              knn_gender,
+                              knn_msi,
+                              test_gender_labels,
+                              test_MSI_labels,
+                              list(data.test_proteomic.index))
+
+
+#Not sure if this should be here in the main.
+
+#All of the other models that are not being trained here for gender right now should be added to this array.
+modelArrayGen = [knn_gender, lr_gender]
+print("\n\n******************************************************************\nHARD VOTE FOR GENDERS")
+hvGender = hv.hard_vote(modelArrayGen, data.proteomic, gender_labels, 'gender')
+print(hvGender)
+print("******************************************************************")
+
+#All of the other models that are not being trained here for msi right now should be added to this array.
+print("\n\n******************************************************************\nHARD VOTE FOR MSI")
+modelArrayMSI = [knn_msi, lr_msi]
+hvMSI = hv.hard_vote(modelArrayMSI, data.proteomic, lr_msi, 'msi')
+print(hvMSI)
+print("******************************************************************")
