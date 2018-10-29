f4f106c67fd8948c0cade16ae18c5854df086edf
LukeSagers <lukewsagers@gmail.com>
2018-10-29 10:11:35 -0600

    added a few more selection things

--- feature_selection.py
+++ feature_selection.py
@@ -1,7 +1,8 @@
 """
 Explore scikitlearn's feature selection capabilities.
 """
-from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_classif
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_classif, SelectFdr, SelectFpr, RFECV, RFE
 from load_data import LoadData
 def _select_features(df, selector=VarianceThreshold, **kwargs):
     """Select features to be used for classification based on some
@@ -29,6 +30,10 @@ def select(df, threshold=0.125):
 if __name__ == "__main__":
     data = LoadData()
     #print(select(data.proteomic, threshold=0.125))
-    selected = SelectKBest(f_classif, k=10).fit(data.proteomic, data.clinical["gender"]+data.clinical["msi"])
+    #selected = SelectKBest(f_classif, k=10).fit(data.proteomic, data.clinical["gender"]+data.clinical["msi"])
+    #selected = SelectFdr(f_classif).fit(data.proteomic, data.clinical["gender"]+data.clinical["msi"])
+    #selected = SelectFpr(f_classif, alpha=0.01).fit(data.proteomic, data.clinical["gender"]+data.clinical["msi"])
+    #selected = RFECV(RandomForestClassifier(), step=0.2).fit(data.proteomic, data.clinical["gender"]+data.clinical["msi"])
+    selected = RFE(RandomForestClassifier(), n_features_to_select=15).fit(data.proteomic, data.clinical["gender"]+data.clinical["msi"])
     df = data.proteomic
     print(df[df.columns[selected.get_support(indices=True)]])
