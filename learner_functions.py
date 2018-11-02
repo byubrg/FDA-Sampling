8a8316e3e443875941bc2956e1432be96b43ef2f
MSBradshaw <michaelscottbradshaw@gmail.com>
2018-11-01 23:44:15 -0600

    created script for predicting mismatches based on model probabilities

--- learner_functions.py
+++ learner_functions.py
@@ -20,6 +20,7 @@ TEST_SIZE = 0.1
 NUMBER_OF_SPLITS = 100
 SCORING_METHOD = 'accuracy'
 
+
 def train_classifier(data, labels, classifier, **kwargs):
     model = classifier(**kwargs)
     cv = StratifiedShuffleSplit(
@@ -36,20 +37,26 @@ def train_classifier(data, labels, classifier, **kwargs):
 def train_rf(data, labels, **kwargs):
     return train_classifier(data, labels, RandomForestClassifier, **kwargs)
 
+
 def train_lr(data, labels):
     return train_classifier(data, labels, LogisticRegression)
+
 
 def train_knn(data,labels, **kwargs):
     return train_classifier(data,labels, KNeighborsClassifier, **kwargs)
 
+
 def train_sgd(data, labels):
     return train_classifier(data,labels, SGDClassifier)
+
 
 def train_sgd_mod(data, labels):
     return train_classifier(data,labels, SGDClassifier, loss='modified_huber')
 
+
 def train_nc(data,labels,**kwargs):
     return train_classifier(data,labels, NearestCentroid,**kwargs)
+
 
 def train_mlp(data, labels):
     return train_classifier(data, labels, MLPClassifier, max_iter=300, solver='sgd')
@@ -65,21 +72,33 @@ def train_bagging_knn(data,labels):
         n_splits = NUMBER_OF_SPLITS,
         test_size = TEST_SIZE)
 
-    scores = cross_val_score(bagging, data, labels, cv = cv, scoring = SCORING_METHOD)
+    scores = cross_val_score(bagging, data, labels, cv=cv, scoring=SCORING_METHOD)
     print(scores)
+
 
 def train_svm(data, labels,**kwargs):
    return train_classifier(data,labels, svm.SVC,**kwargs)
 
+
 def make_test_prediction(model, data, labels, print_details=True):
     pred = model.predict(data)
-    if(print_details):
-        print('score', accuracy_score(pred, labels))
-        print('pred', pred)
-        print('actual', labels)
-        print(confusion_matrix(labels,pred))
+    probs = model.predict_proba(data)
+    print('Predictions:', pred)
+    print('Probabilies:', probs)
+    # if print_details:
+    #     print('score', accuracy_score(pred, labels))
+    #     print('pred', pred)
+    #     print('actual', labels)
+    #     print(confusion_matrix(labels,pred))
 
     return pred
+
+
+def get_prediction_and_prob(model, data):
+    pred = model.predict(data)
+    probs = model.predict_proba(data)
+    return pred, probs
+
 
 """
 given the protein data, two model trained for gender and msi classification and the final sample names
@@ -104,6 +123,29 @@ def generate_and_write_results(pro_data, model_gender, model_msi, gender_labels,
 
 
 def generate_and_write_results_hard_voting(pro_data, model_gender, model_msi, gender_labels, msi_labels, sample_names, strict=True):
+    gender_predictions = hv.hard_vote(model_gender, pro_data, gender_labels, 'gender')
+
+    msi_predictions = hv.hard_vote(model_msi, pro_data, msi_labels, 'msi')
+    outfile = open('subchallenge_1.csv','w')
+    outfile.write('sample,mismatch\n')
+
+    if strict:
+        for i in range(0,len(msi_labels)):
+            outfile.write(sample_names[i] + ',')
+            if gender_labels[i] == gender_predictions[i] and msi_labels[i] == msi_predictions[i]:
+                outfile.write('0\n')
+            else:
+                outfile.write('1\n')
+    else:
+        for i in range(0, len(msi_labels)):
+            outfile.write(sample_names[i] + ',')
+            if gender_labels[i] != gender_predictions[i] and msi_labels[i] != msi_predictions[i]:
+                outfile.write('1\n')
+            else:
+                outfile.write('0\n')
+    outfile.close()
+
+def generate_and_write_probability_voting(pro_data, model_gender, model_msi, gender_labels, msi_labels, sample_names, strict=True):
     gender_predictions = hv.hard_vote(model_gender, pro_data, gender_labels, 'gender')
 
     msi_predictions = hv.hard_vote(model_msi, pro_data, msi_labels, 'msi')
