015ee90bac33c1c0020253da34e6d56982508971
Jonathan Dayton <jonathandayton23@gmail.com>
2018-11-12 11:49:18 -0700

    Get functional siamese network

---
+++ siamese_net.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+from keras.layers import Input, Dense, concatenate
+from keras.models import Model
+
+from load_data import LoadData
+
+if __name__ == "__main__":
+    data = LoadData()
+
+    proteomic = Input((data.proteomic.shape[-1],))
+    p1 = Dense(256, activation='relu')(proteomic)
+
+    rna = Input((data.rna.shape[-1],))
+    r1 = Dense(256, activation='relu')(rna)
+
+    x = concatenate([p1, r1])
+    x = Dense(64, activation='relu')(x)
+    x = Dense(8, activation='relu')(x)
+
+    output = Dense(1, activation='sigmoid')(x)
+
+    model = Model(inputs=[proteomic, rna], outputs=[output])
+
+    model.compile(optimizer='adam', loss='binary_crossentropy')
+
+    prot_x = pd.concat([data.proteomic, data.proteomic])
+    shuffled_rna = data.rna.sample(frac=1)
+    rna_x = pd.concat([data.rna, shuffled_rna])
+    labels = [1.0] * 80 + [0.0] * 80
+
+    model.fit([prot_x, rna_x], [labels], epochs=10, batch_size=8)
