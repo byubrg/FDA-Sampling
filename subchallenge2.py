eaa82b0ce747531fa1b43c5ef36d32269947d8dd
MSBradshaw <michaelscottbradshaw@gmail.com>
2018-11-27 23:01:02 -0700

    added comments describing functions that need to be built

--- subchallenge2.py
+++ subchallenge2.py
@@ -15,4 +15,15 @@ mismatch_labels = data.mismatch['mismatch'].tolist()
 # train models for predicting mislabels based on all 3 data sets
 lf.train_rf(data.train_all.fillna(0), data.mislabel_labels)
 
+# do feature selection
+# create a single function that returns just the most important features from protein, rna and clinical data sets
+#
+
+# get mislabeled samples
+# create a function to get list of samples that have mislabels return list of row indexes
+
 # do deconvolution
+# given a list of row indexes of samples that are mislabeled, correct the mislabeling
+#   find which of the 3 types of data does not belong, rna, protein or clinical
+#   brute force all combinations of similarly mismatched data do find best match
+#   generate report
