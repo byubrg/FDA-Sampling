274690ef9bc3c7581e7166183397217923345048
LukeSagers <lukewsagers@gmail.com>
2018-12-05 11:35:15 -0700

    Add feature selection for siamese net.

--- siamese_net.py
+++ siamese_net.py
@@ -4,14 +4,16 @@ from keras.layers import Input, Dense, concatenate
 from keras.models import Model
 
 from load_data import LoadData
+import feature_selection 
 
 if __name__ == "__main__":
     data = LoadData()
-
-    proteomic = Input((data.proteomic.shape[-1],))
+    pro_data = feature_selection.univariate(data.proteomic, data.clinical)
+    proteomic = Input((pro_data.shape[-1],))
     p1 = Dense(256, activation='relu')(proteomic)
 
-    rna = Input((data.rna.shape[-1],))
+    rna_data = feature_selection.univariate(data.rna, data.clinical)
+    rna = Input((rna_data.shape[-1],))
     r1 = Dense(256, activation='relu')(rna)
 
     x = concatenate([p1, r1])
@@ -24,14 +26,14 @@ if __name__ == "__main__":
 
     model.compile(optimizer='adam', loss='binary_crossentropy')
 
-    prot_x = pd.concat([data.proteomic, data.proteomic])
-    shuffled_rna = data.rna.sample(frac=1)
-    rna_x = pd.concat([data.rna, shuffled_rna])
+    prot_x = pd.concat([pro_data, pro_data])
+    shuffled_rna = rna_data.sample(frac=1)
+    rna_x = pd.concat([rna_data, shuffled_rna])
     labels = [1.0] * 80 + [0.0] * 80
 
-    model.fit([prot_x, rna_x], [labels], epochs=50, batch_size=2)
+    model.fit([prot_x, rna_x], [labels], epochs=20, batch_size=5)
 
     truth = pd.read_csv("./data/tidy/sum_tab_2.csv")
-    truth['Score'] = model.predict([data.proteomic, data.rna])
+    truth['Score'] = model.predict([pro_data, rna_data])
 
     truth.to_csv("./data/tidy/output/siamese_scores.csv", index=False)
