7b41f58db66e0eae519494caf652a1bf3d3efe65
Jonathan Dayton <jonathandayton23@gmail.com>
2018-12-12 22:14:47 -0700

    Use improved crossover algorithm

--- genetic.py
+++ genetic.py
@@ -21,14 +21,42 @@ def sum_prob(truth, individual):
         tot += float(score1["Probability"]) + float(score2["Probability"])
     return tot
 
-def crossover(seq1, seq2, crossover_index):
-    offspring = seq1[:crossover_index]
-    used = set(offspring)
-    for item in seq2:
-        if item not in used:
-            offspring += [item]
-        used.add(item)
-    return offspring
+def cycle_crossover(seq1, seq2):
+    """Cycle crossover technique for maintaining position-based genes.
+    Idea came from https://stackoverflow.com/a/14423240/6481442 , and
+    implementation details came from
+    http://www.rubicite.com/Tutorials/GeneticAlgorithms/CrossoverOperators/CycleCrossoverOperator.aspx
+    """
+    ## Find the cycles
+    visited_indices = set()
+    d1 = {patient: index for index, patient in enumerate(seq1)}
+    d2 = {patient: index for index, patient in enumerate(seq2)}
+    cycles = []
+    for i, (p1, p2) in enumerate(zip(seq1, seq2)):
+        if i in visited_indices:
+            continue
+        cycle = [i]
+        visited_indices.add(i)
+        cycle_start = p1
+        match = p2
+        visited_indices.add(cycle_start)
+        while match != cycle_start:
+            index = d1[match]
+            cycle.append(index)
+            visited_indices.add(index)
+            match = seq2[index]
+        cycles.append(cycle)
+    ## Cross them over
+    child = [None] * len(seq1)
+    parent = seq2
+    for cycle in cycles:
+        for index in cycle:
+            child[index] = parent[index]
+        if parent == seq1:
+            parent = seq2
+        else:
+            parent = seq1
+    return child
 
 def mutate(seq):
     seq = seq[:]
@@ -58,8 +86,7 @@ if __name__ == "__main__":
         best_genes = fittest[-1]
 
         # Cross over best individuals
-        halfway = int(len(patients) / 2)
-        offspring = [crossover(choice(fittest), choice(fittest), halfway) for _ in range(POPULATION_SIZE)]
+        offspring = [cycle_crossover(choice(fittest), choice(fittest)) for _ in range(POPULATION_SIZE)]
 
         # Introduce random variation
         population = [mutate(individual) for individual in offspring]
