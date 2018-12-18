509aa55519487b8b50bd1e06f4e5ffbf90e979e5
Jonathan Dayton <jonathandayton23@gmail.com>
2018-12-18 11:55:33 -0700

    Get genetic algorithm working with proteomic, rna, and clinical data.

--- genetic.py
+++ genetic.py
@@ -1,35 +1,114 @@
 import pandas as pd
 from random import sample, choice, randrange
-from time import time
+from probabilities import clinical_labels_dict
 
 POPULATION_SIZE = 1000
 N_FITTEST = 100
 N_GENERATIONS = 100
 N_MUTATIONS = 1
 
+class Individual(object):
+    def __init__(self, clinical=None, proteomic=None, rna=None):
+        self.clinical = clinical
+        self.proteomic = proteomic
+        self.rna = rna
+
+    def copy(self):
+        return Individual(self.clinical[:], self.proteomic[:], self.rna[:])
+
+    def dataframe(self):
+        return pd.DataFrame({
+            "clinical": self.clinical,
+            "proteomic": self.proteomic,
+            "rna": self.rna
+        }).sort_values("clinical").reset_index(drop=True)
+
+    def mutate(self):
+        return Individual(mutate(self.clinical), mutate(self.proteomic), mutate(self.rna))
+
+class Genetic(object):
+    def __init__(self):
+        rna_prot_df = pd.read_csv("./data/probabilities/rna_proteomic.csv", index_col=0)
+        self.rna_prot = {}
+        for i, row in rna_prot_df.iterrows():
+            for prot, prob in row.iteritems():
+                rna = row.name
+                if rna not in self.rna_prot:
+                    self.rna_prot[rna] = {}
+                self.rna_prot[rna][prot] = prob
+        rna_clin_df = pd.read_csv("./data/probabilities/clinical_rna.csv", index_col=0)
+        self.rna_clin = {}
+        for i, row in rna_clin_df.iterrows():
+            self.rna_clin[row.name] = row.tolist()
+        prot_clin_df = pd.read_csv("./data/probabilities/clinical_proteomic.csv", index_col=0)
+        self.prot_clin = {}
+        for i, row in prot_clin_df.iterrows():
+            self.prot_clin[row.name] = row.tolist()
+        self.patients = prot_clin_df.index.tolist()
+        self.clin = clinical_labels_dict()
+        self.initialize_population()
+
+    def mutate(self, individual):
+        return individual.mutate()
+
+    def crossover(self, parent1: Individual, parent2: Individual) -> Individual:
+        clinical = cycle_crossover(parent1.clinical, parent2.clinical)
+        proteomic = cycle_crossover(parent1.proteomic, parent2.proteomic)
+        rna = cycle_crossover(parent1.rna, parent2.rna)
+        return Individual(clinical=clinical, proteomic=proteomic, rna=rna)
+
+    def fitness(self, individual, matching_bonus=1.1):
+        score = 0.0
+        for clin, prot, rna in zip(individual.clinical, individual.proteomic, individual.rna):
+            prot_clin_label = int(self.clin[prot])
+            rna_clin_label = int(self.clin[rna])
+            if clin != prot and clin != rna and prot != rna:
+                score -= 1
+            rna_prot_score = self.rna_prot[rna][prot]
+            rna_prot_score *= matching_bonus if rna == prot else 1.0
+            rna_clin_score = float(self.rna_clin[clin][rna_clin_label])
+            rna_clin_score *= matching_bonus if rna == clin else 1.0
+            prot_clin_score = float(self.prot_clin[clin][prot_clin_label])
+            prot_clin_score *= matching_bonus if prot == clin else 1.0
+            score += rna_prot_score + rna_clin_score + prot_clin_score
+        return score
+
+    def initialize_population(self):
+        self.population = [
+            Individual(*[
+                sample(self.patients, len(self.patients)) for _ in range(3)
+            ]) for _ in range(POPULATION_SIZE)
+        ]
+
+    def train(self):
+        best_score = 0.0
+        for generation in range(N_GENERATIONS):
+            if generation > N_GENERATIONS * 0.95:
+                # Train without mutations for the last 5 of generations.%.
+                N_MUTATIONS = 0
+            # Calc scores
+            scores = [self.fitness(individual) for individual in self.population]
+
+            # Select best individuals
+            df = pd.DataFrame({"genes": self.population, "score": scores})
+            fittest_df = df.sort_values(["score"]).tail(N_FITTEST)
+
+            print("Generation {}. Best score: {}".format(generation, fittest_df['score'].tolist()[-1]))
+
+            fittest = fittest_df['genes'].tolist()
+            if fittest_df['score'].tolist()[-1] > best_score:
+                self.best_genes = fittest[-1]
+            fittest.append(self.best_genes)
+
+            # Cross over best individuals
+            offspring = [self.crossover(choice(fittest), choice(fittest)) for _ in range(POPULATION_SIZE)]
+
+            # Introduce random variation
+            self.population = [self.mutate(individual) for individual in offspring]
+
 pairwise_scores = pd.read_csv("data/tidy/output/pairwise_scores.csv", index_col=0)
-score_lookup = {}
-for i, row in pairwise_scores.iterrows():
-    rna = str(row["RNA"])
-    prot = str(row["Proteomic"])
-    prob = float(row["Probability"])
-    if rna not in score_lookup:
-        score_lookup[rna] = {}
-    score_lookup[rna][prot] = prob
 
-def sum_prob(truth, individual):
-    tot = 0.0
-    for p1, p2 in zip(truth, individual):
-        score1 = score_lookup[p1][p2]
-        score2 = score_lookup[p2][p1]
-        if p1 == p2:
-            # Add reward for matching the samples with their original labels
-            # proportional to their siamese network outputs.
-            tot += score1 * 0.1
-        tot += score1 + score2
-    return tot
-
-def cycle_crossover(seq1, seq2):
+def cycle_crossover(parent1, parent2):
     """Cycle crossover technique for maintaining position-based genes.
     Idea came from https://stackoverflow.com/a/14423240/6481442 , and
     implementation details came from
@@ -37,33 +116,32 @@ def cycle_crossover(seq1, seq2):
     """
     ## Find the cycles
     visited_indices = set()
-    d1 = {patient: index for index, patient in enumerate(seq1)}
-    d2 = {patient: index for index, patient in enumerate(seq2)}
+    d1 = {patient: index for index, patient in enumerate(parent1)}
+    d2 = {patient: index for index, patient in enumerate(parent2)}
     cycles = []
-    for i, (p1, p2) in enumerate(zip(seq1, seq2)):
+    for i, (p1, p2) in enumerate(zip(parent1, parent2)):
         if i in visited_indices:
             continue
         cycle = [i]
         visited_indices.add(i)
         cycle_start = p1
         match = p2
-        visited_indices.add(cycle_start)
         while match != cycle_start:
             index = d1[match]
             cycle.append(index)
             visited_indices.add(index)
-            match = seq2[index]
+            match = parent2[index]
         cycles.append(cycle)
     ## Cross them over
-    child = [None] * len(seq1)
-    parent = seq2
+    child = [None] * len(parent1)
+    parent = parent2
     for cycle in cycles:
         for index in cycle:
             child[index] = parent[index]
-        if parent == seq1:
-            parent = seq2
+        if parent == parent1:
+            parent = parent2
         else:
-            parent = seq1
+            parent = parent1
     return child
 
 def mutate(seq):
@@ -76,29 +154,11 @@ def mutate(seq):
 
 if __name__ == "__main__":
     # Generate initial population
-    patients = pairwise_scores["RNA"].drop_duplicates().tolist()
-    print("Score to beat: {}".format(sum_prob(patients, patients)))
-    population = [sample(patients, len(patients)) for _ in range(POPULATION_SIZE)]
+    genetic = Genetic()
+    no_mismatches = Individual(genetic.patients, genetic.patients, genetic.patients)
+    print("Score to beat: {}".format(genetic.fitness(no_mismatches)))
 
-    for generation in range(N_GENERATIONS):
-        # Calc scores
-        scores = [sum_prob(patients, individual) for individual in population]
-
-        # Select best individuals
-        df = pd.DataFrame({"genes": population, "score": scores})
-        fittest = df.sort_values(["score"]).tail(N_FITTEST)
-
-        print("Generation {}. Best score: {}".format(generation, fittest['score'].tolist()[-1]))
-
-        fittest = fittest['genes'].tolist()
-        best_genes = fittest[-1]
-
-        # Cross over best individuals
-        offspring = [cycle_crossover(choice(fittest), choice(fittest)) for _ in range(POPULATION_SIZE)]
-
-        # Introduce random variation
-        population = [mutate(individual) for individual in offspring]
+    genetic.train()
 
     print("Best arrangement found:")
-    for p1, p2 in zip(patients, best_genes):
-        print("{},{}".format(p1, p2))
+    print(genetic.best_genes.dataframe())
--- probabilities.py
+++ probabilities.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from load_data import LoadData
-from learner_functions import train_rf
+import learner_functions
+from siamese_net import SiameseNet
 
 CLINICAL_VALUES = {
     ("Female", "MSI-Low/MSS"): 0,
@@ -9,7 +10,21 @@ CLINICAL_VALUES = {
     ("Male",   "MSI-High"):    3,
 }
 
-def clinical_probabilities():
+def clinical_labels_dict():
+    data = LoadData()
+
+    def clinical_to_int(row):
+        output = 0
+        if row.msi == "MSI-High":
+            output += 2
+        if row.gender == "Female":
+            output += 1
+        return output
+
+    labels = data.clinical.apply(clinical_to_int, axis="columns")
+    return {sample: label for sample, label in zip(data.clinical.index, labels)}
+
+def clinical_probabilities(learner=learner_functions.train_rf):
     """Get the probabilities of each clinical class for each sample for
     proteomic and rna data.
 
@@ -27,10 +42,55 @@ def clinical_probabilities():
 
     labels = data.clinical.apply(clinical_to_int, axis="columns")
 
-    rf = train_rf(data.proteomic, labels)
-    proteomic_probabilities_df = pd.DataFrame(rf[0].predict_proba(data.proteomic))
+    model = learner(data.proteomic, labels)
+    proteomic_probabilities_df = pd.DataFrame(model[0].predict_proba(data.proteomic))
+    proteomic_probabilities_df['sample'] = data.clinical.index
+    proteomic_probabilities_df = proteomic_probabilities_df.set_index('sample')
 
-    rf = train_rf(data.rna, labels)
-    rna_probabilities_df = pd.DataFrame(rf[0].predict_proba(data.rna))
+    model = learner(data.rna, labels)
+    rna_probabilities_df = pd.DataFrame(model[0].predict_proba(data.rna))
+    rna_probabilities_df['sample'] = data.clinical.index
+    rna_probabilities_df = rna_probabilities_df.set_index('sample')
 
     return proteomic_probabilities_df, rna_probabilities_df
+
+def rna_proteomic_mismatch_probabilities():
+    # Train siamese network
+    data = LoadData()
+    pro_data = data.proteomic
+    rna_data = data.rna
+
+    prot_x = pd.concat([pro_data, pro_data, pro_data])
+    shuffled_rna = rna_data.sample(frac=1)
+    rna_x = pd.concat([rna_data, shuffled_rna, rna_data.sample(frac=1)])
+    labels = [1.0] * 80 + [0.0] * 160
+
+    network = SiameseNet([
+        (pro_data.shape[-1],),
+        (rna_data.shape[-1],)
+    ])
+    network.fit([prot_x, rna_x], labels, epochs=100, batch_size=5, verbose=False)
+
+    # Calculate pairwise probabilities
+    vals = {
+        "Proteomic": [],
+        "RNA": [],
+        "Probability": [],
+    }
+    for i, x in pro_data.iterrows():
+        for j, y in rna_data.iterrows():
+            vals['Proteomic'].append(x.name)
+            vals['RNA'].append(y.name)
+            vals['Probability'].append(network.predict([[x], [y]])[0][0])
+
+    probs = pd.DataFrame(vals)
+    order = data.clinical.index.tolist()
+    probs = probs.pivot(index='RNA', columns='Proteomic', values='Probability')[order].reindex(order)
+    return probs
+
+if __name__ == "__main__":
+    prot_clinical, rna_clinical = clinical_probabilities()
+    rna_prot = rna_proteomic_mismatch_probabilities()
+    prot_clinical.to_csv("./data/probabilities/clinical_proteomic.csv")
+    rna_clinical.to_csv("./data/probabilities/clinical_rna.csv")
+    rna_prot.to_csv("./data/probabilities/rna_proteomic.csv")
